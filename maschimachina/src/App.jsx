af590fc30b4ed669d8f0e97ef0f94b4335898a01
Tomas Peters <tdpeters6794@gmail.com>
2024-03-09 13:51:55 -0500

    trying to get this jquery plugin to work

--- maschimachina/src/App.jsx
+++ maschimachina/src/App.jsx
@@ -12,7 +12,19 @@ function App() {
         <Route path="/collections" element={<Collections />} />
         <Route path="/iMaschination" element={<Gallery />} />
         <Route path="/onetimeonly" element={<Gallery />} />
-        <Route path="/theworldawake" element={<Gallery />} />
+        <Route
+          path="/theworldawake"
+          element={
+            <Gallery
+              galleryName="The World Awake"
+              galleryText="All four mountings reflect modern architecture, evidenced in calibrated contours, defined edges,
+and understated ornament. To breathe life into the muted surfaces and pointed construction, I
+selected contrasting, verdant stones and pearls. These precious materials offer associations of
+prosperity and protection, with hues emphasizing the new.
+"
+            />
+          }
+        />
         <Route path="/contact" element={<Contact />} />
       </Routes>
     </>
--- maschimachina/src/components/Gallery.jsx
+++ maschimachina/src/components/Gallery.jsx
@@ -1,7 +1,43 @@
-export default function Gallery() {
+import Header from "./Header";
+import Footer from "./Footer";
+import "./Gallery.css";
+import twaimg1 from "../assets/the-world-awake-images/IMG_8288.png";
+import twaimg2 from "../assets/the-world-awake-images/IMG_8376.png";
+import twaimg3 from "../assets/the-world-awake-images/IMG_8384.png";
+import twaimg4 from "../assets/the-world-awake-images/IMG_8450.png";
+import twaimg5 from "../assets/the-world-awake-images/IMG_8591.png";
+import twaimg6 from "../assets/the-world-awake-images/IMG_8610.png";
+
+export default function Gallery({ galleryName, galleryText }) {
   return (
-    <>
-      <p>Hello World</p>
-    </>
+    <div className="gallery-content">
+      <Header />
+      <div className="gallery-header">
+        <div className="gallery-name">{galleryName}</div>
+        <div className="gallery-description">{galleryText}</div>
+      </div>
+      <div className="media-scroller snaps-inline">
+        <div className="media-element">
+          <img className="scrolled-photo" src={twaimg1}></img>
+        </div>
+        <div className="media-element">
+          <img className="scrolled-photo" src={twaimg2}></img>
+        </div>
+        <div className="media-element">
+          <img className="scrolled-photo" src={twaimg3}></img>
+        </div>
+        <div className="media-element">
+          <img className="scrolled-photo" src={twaimg4}></img>
+        </div>
+        <div className="media-element">
+          <img className="scrolled-photo" src={twaimg5}></img>
+        </div>
+        <div className="media-element">
+          <img className="scrolled-photo" src={twaimg6}></img>
+        </div>
+      </div>
+
+      <Footer />
+    </div>
   );
 }
